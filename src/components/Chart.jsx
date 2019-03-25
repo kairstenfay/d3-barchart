00fff7f24526d21786c9f353be4440a741f53b1d
KairstenFay <kairsten.fay@gmail.com>
2019-03-24 18:16:19 -0700

    Newly styled tooltip added back in with all tests passing. No idea how the last bug appeared

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -63,18 +63,41 @@ export default class Chart extends React.Component{
 function renderToolTip(attributes) {
     if (attributes && attributes['data-date']) {
         const dataDate = attributes['data-date'].nodeValue;
+        const dataGDP = attributes['data-gdp'].nodeValue;
+
         const x = attributes.x.nodeValue; // todo import padding
         const y = attributes.y.nodeValue + 90; // todo import padding
+        const triangleWidth = 10; // todo import from styles or put in config
 
+        const polygonPoints = `${x - triangleWidth},${y - triangleWidth} 
+                               ${x},${y - 1}
+                               ${x + triangleWidth},${y - triangleWidth}
+                               `;
+
+        const rectangleWidth = 170;
+        const rectangleHeight = 40;
         return (
 
             <g>
-                <rect x={x} y={y - 20 - 20} width="100" height="30" fill="salmon" />
-                <text id="tooltip" data-date={dataDate} x={x} y={y - 20} fill="black" >
+                <rect x={x - triangleWidth - 0.5 * rectangleWidth}
+                      y={y - triangleWidth - rectangleHeight}
+                      width={rectangleWidth}
+                      height={rectangleHeight}
+                      fill="black" />
+                <text id="tooltip" className="date-tooltip-text" data-date={dataDate}
+                      x={x - 0.5 * rectangleWidth}
+                      y={y - 30}
+                      fill="white" >
                     {dataDate}
                 </text>
+                <text className="gdp-tooltip-text"
+                      x={x - 0.5 * rectangleWidth}
+                      y={y - 15}
+                      fill="white" >
+                    ${dataGDP} billion USD
+                </text>
+                <polygon points={polygonPoints} />
 
-                <circle cx={x} cy={y} r="5" fill="none" stroke="black" strokeWidth="2" />
             </g>
         );
     }
